110598fe8f3579d5cd7a64d6240f98bbfcfb766e
Moritz Münch <moritz.muench@mni.thm.de>
2020-11-26 21:59:45 +0100

    Replaced icons with NerdFont icons

--- src/weather.rs
+++ src/weather.rs
@@ -95,9 +95,9 @@ impl Output {
             temp_fahrenheit: current.temperature.as_unit(Unit::Fahrenheit).0,
             temp_icon: current.icon,
             trend: match (current.temperature, forecast.temperature) {
-                (c, f) if c < f => '',
-                (c, f) if c > f => '',
-                _ => '',
+                (c, f) if c < f => '勤',
+                (c, f) if c > f => '免',
+                _ => '勉',
             },
             forecast_celcius: forecast.temperature.0,
             forecast_kelvin: forecast.temperature.as_unit(Unit::Kelvin).0,
@@ -114,23 +114,23 @@ impl Output {
 // More details here: https://openweathermap.org/weather-conditions
 fn get_icon(code: &str) -> char {
     match code {
-        "01d" => '',         // Clear sky - day
-        "01n" => '',         // Clear sky - night
-        "02d" => '',         // Few clouds (11-25%) - day
-        "02n" => '',         // Few clouds (11-25%) - night
-        "03d" | "03n" => '', // Scattered clouds (25-50%) - day/night
-        "04d" | "04n" => '', // Broken / Overcast clouds (51-84% / 85-100%) - day/night
-        "09d" => '',         // Shower rain - day
-        "09n" => '',         // Shower rain - night
-        "10d" => '',         // Moderate / heavy rain - day
-        "10n" => '',         // Moderate / heavy rain - night
-        "11d" => '',         // Thunderstorm - day
-        "11n" => '',         // Thunderstorm - night
-        "13d" => '',         // Snow - day
-        "13n" => '',         // Snow - night
-        "50d" => '',         // Fog - day
-        "50n" => '',         // Fog - night
-        _ => '',             // ??
+        "01d" => '',         // Clear sky - day
+        "01n" => '',         // Clear sky - night
+        "02d" => '',         // Few clouds (11-25%) - day
+        "02n" => '',         // Few clouds (11-25%) - night
+        "03d" | "03n" => '摒', // Scattered clouds (25-50%) - day/night
+        "04d" | "04n" => '', // Broken / Overcast clouds (51-84% / 85-100%) - day/night
+        "09d" => '',         // Shower rain - day
+        "09n" => '',         // Shower rain - night
+        "10d" => '',         // Moderate / heavy rain - day
+        "10n" => '',         // Moderate / heavy rain - night
+        "11d" => '',         // Thunderstorm - day
+        "11n" => '',         // Thunderstorm - night
+        "13d" => '',         // Snow - day
+        "13n" => '',         // Snow - night
+        "50d" => '',         // Fog - day
+        "50n" => '',         // Fog - night
+        _ => '',             // ??
     }
 }
 
